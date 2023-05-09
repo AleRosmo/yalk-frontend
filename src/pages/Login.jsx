7b664f136cc0cfb0b74dc099903e02871e23123b
Revengeic3 <onair95@gmail.com>
2023-05-09 16:34:24 +0200

    Berak down login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,10 +14,58 @@ import {
     useColorModeValue,
 } from '@chakra-ui/react';
 import React, { useLayoutEffect, useState } from 'react';
+import AuthService from '../services/auth.service';
+
 export default function Login() {
   const [emailInput, setEmailInput] = useState();
   const [passwordInput, setPasswordInput] = useState();
   const [rememberMe, setRememberMe] = useState();
+
+  const LoginForm = () => (
+    <Stack spacing={4}>
+      <FormControl id="email">
+        <FormLabel>Email address</FormLabel>
+        <Input
+          color={'gray.200'}
+          bgColor={'gray.600'}
+          type="email"
+          onChange={setEmailInput}
+        />
+      </FormControl>
+      <FormControl id="password">
+        <FormLabel>Password</FormLabel>
+        <Input
+          color={'gray.200'}
+          bgColor={'gray.600'}
+          type="password"
+          onChange={setPasswordInput}
+        />
+      </FormControl>
+      <Stack spacing={10}>
+        <Stack
+          direction={{
+            base: 'column',
+            sm: 'row',
+          }}
+          align={'start'}
+          justify={'space-between'}
+        >
+          <Checkbox onChange={setRememberMe}>{'Remember me'}</Checkbox>
+          <Link color={'teal.400'}>Forgot password?</Link>
+        </Stack>
+        <Button
+          bg={'teal.400'}
+          color={'white'}
+          _hover={{
+            bg: 'teal.300',
+          }}
+          onClick={() => AuthService.login(emailInput, passwordInput)}
+        >
+          Sign in
+        </Button>
+      </Stack>
+    </Stack>
+  );
 
   useLayoutEffect(() => {
     // Here the login check before
@@ -34,38 +82,22 @@ export default function Login() {
       color={'teal'}
     >
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>{'Ÿalk!'}</Heading>
-          <Text fontSize={'lg'}>
-            {'palle '}
-            <Link color={'teal.400'}>{'floscissime'}</Link>
-            {' ✌️ ma davvero tanto raga un botto devo spaziare'}
-          </Text>
-        </Stack>
+        <LoginHeader />
         <Box rounded={'lg'} bg={'gray.700'} boxShadow={'lg'} p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input color={'gray.200'} bgColor={'gray.600'} type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input color={'gray.200'} bgColor={'gray.600'} type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}
-              >
-                <Checkbox>{'Remember me'}</Checkbox>
-                <Link color={'teal.400'}>Forgot password?</Link>
-              </Stack>
-              <Button bg={"teal.400"} color={"white"} _hover={{bg:"teal.300"}}>Sign in</Button>
-            </Stack>
-          </Stack>
+          <LoginForm />
         </Box>
       </Stack>
     </Flex>
   );
 }
+
+const LoginHeader = () => (
+  <Stack align={'center'}>
+    <Heading fontSize={'4xl'}>{'Ÿalk!'}</Heading>
+    <Text fontSize={'lg'}>
+      {'palle '}
+      <Link color={'teal.400'}>{'floscissime'}</Link>
+      {' ✌️ ma davvero tanto raga un botto devo spaziare'}
+    </Text>
+  </Stack>
+);
