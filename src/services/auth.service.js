b4c8c5b214446ab59111a907730fe6f272a0348c
Revengeic3 <onair95@gmail.com>
2023-05-20 08:31:07 +0200

    Switch to returning promises from auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,48 +1,55 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "/auth";
+const API_URL = '/auth';
 
-const login = async (email, password) => {
-	const response = await axios.post(
-		API_URL + "/signin",
-		{ email, password },
-		{
-			withCredentials: true,
-		}
-	);
-	if (response.data.sender) {
-		localStorage.setItem("user", JSON.stringify(response.data));
-	}
-	return response.data;
-};
+const login = (email, password) =>
+  axios.post(
+    API_URL + '/signin',
+    { email, password },
+    {
+      withCredentials: true,
+    }
+  );
+//   if (response.data.sender) {
+//     localStorage.setItem('user', JSON.stringify(response.data));
+//   }
 
 const logout = async () => {
-	const response = await axios.get(API_URL + "/signout", {
-		withCredentials: true,
-	});
-	localStorage.removeItem("user");
-	return response.data;
+  const response = await axios.get(API_URL + '/signout', {
+    withCredentials: true,
+  });
+  localStorage.removeItem('user');
+  return response.data;
 };
 
-const validate = async () => {
-	const response = await axios.get(API_URL, {
-		withCredentials: true,
-	});
-	if (response.status === 200) {
-		return true;
-	}
-	return false;
+const validate = () => {
+  return axios.get(API_URL, {
+    withCredentials: true,
+  });
+};
+
+const getToken = () => {
+  const tokenName = 'YWS';
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(tokenName + '=')) {
+      return cookie.substring(tokenName.length + 1);
+    }
+  }
+  return null;
 };
 
 const getCurrentUser = () => {
-	return JSON.parse(localStorage.getItem("user"));
+  return JSON.parse(localStorage.getItem('user'));
 };
 
 const AuthService = {
-	login,
-	logout,
-	validate,
-	getCurrentUser,
+  login,
+  logout,
+  validate,
+  getCurrentUser,
+  getToken,
 };
 
 export default AuthService;
