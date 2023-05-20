373bf6ffc9e97c2dcad04c6fdf05d13edd259dfd
Revengeic3 <onair95@gmail.com>
2023-05-20 08:34:12 +0200

    Manage authentication inside the component for error display

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,76 +1,110 @@
 import {
-    Box,
-    Button,
-    Checkbox,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    Link,
-    Stack,
-    Text,
-    color,
-    useColorModeValue,
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+  color,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import React, { useLayoutEffect, useState } from 'react';
-import AuthService from '../services/auth.service';
+import { useNavigate } from 'react-router-dom';
+import { default as AuthService } from '../services/auth.service';
 
 export default function Login() {
-  const [emailInput, setEmailInput] = useState();
-  const [passwordInput, setPasswordInput] = useState();
-  const [rememberMe, setRememberMe] = useState();
+  const [invalidSession, setInvalidSession] = useState();
+  const navigate = useNavigate();
 
-  const LoginForm = () => (
-    <Stack spacing={4}>
-      <FormControl id="email">
-        <FormLabel>Email address</FormLabel>
-        <Input
-          color={'gray.200'}
-          bgColor={'gray.600'}
-          type="email"
-          onChange={setEmailInput}
-        />
-      </FormControl>
-      <FormControl id="password">
-        <FormLabel>Password</FormLabel>
-        <Input
-          color={'gray.200'}
-          bgColor={'gray.600'}
-          type="password"
-          onChange={setPasswordInput}
-        />
-      </FormControl>
-      <Stack spacing={10}>
-        <Stack
-          direction={{
-            base: 'column',
-            sm: 'row',
-          }}
-          align={'start'}
-          justify={'space-between'}
-        >
-          <Checkbox onChange={setRememberMe}>{'Remember me'}</Checkbox>
-          <Link color={'teal.400'}>Forgot password?</Link>
+  const LoginForm = () => {
+    const [emailInput, setEmailInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+    const [rememberMe, setRememberMe] = useState();
+
+    useLayoutEffect(() => {
+      const token = AuthService.getToken();
+      if (!token) {
+        setInvalidSession(false);
+      }
+      AuthService.validate()
+        .then(res => {
+          if (res.status === 200) {
+            navigate('/chat/1');
+          }
+        })
+        .catch(err => {
+          if (err.status === 401) {
+            setInvalidSession(true);
+          }
+        });
+    });
+
+    async function handleLogin() {
+      const redirectUrl = await AuthService.login(emailInput, passwordInput)
+        .then(res => {
+          if (res.status === 200) {
+            navigate('/chat/1');
+          }
+        })
+        .catch(() => {
+          setInvalidSession(true);
+        });
+    }
+
+    return (
+      <Stack spacing={4}>
+        <FormControl id="email">
+          <FormLabel>Email address</FormLabel>
+          <Input
+            color={'gray.200'}
+            bgColor={'gray.600'}
+            type="email"
+            onChange={e => setEmailInput(e.target.value)}
+          />
+        </FormControl>
+        <FormControl id="password">
+          <FormLabel>Password</FormLabel>
+          <Input
+            color={'gray.200'}
+            bgColor={'gray.600'}
+            type="password"
+            onChange={e => setPasswordInput(e.target.value)}
+          />
+        </FormControl>
+        <Stack spacing={10}>
+          <Stack
+            direction={{
+              base: 'column',
+              sm: 'row',
+            }}
+            align={'start'}
+            justify={'space-between'}
+          >
+            <Checkbox onChange={e => setRememberMe(e.target.value)}>
+              {'Remember me'}
+            </Checkbox>
+            <Link color={'teal.400'}>Forgot password?</Link>
+          </Stack>
+          {invalidSession ? <Text>Not logged in</Text> : null}
+          <Button
+            bg={'teal.400'}
+            color={'white'}
+            _hover={{
+              bg: 'teal.300',
+            }}
+            onClick={() => handleLogin()}
+          >
+            Sign in
+          </Button>
         </Stack>
-        <Button
-          bg={'teal.400'}
-          color={'white'}
-          _hover={{
-            bg: 'teal.300',
-          }}
-          onClick={() => AuthService.login(emailInput, passwordInput)}
-        >
-          Sign in
-        </Button>
       </Stack>
-    </Stack>
-  );
-
-  useLayoutEffect(() => {
-    // Here the login check before
-    // Possbily loader of react router?
-  });
+    );
+  };
 
   return (
     // In this flex we need the use the hook useColorModeValue
