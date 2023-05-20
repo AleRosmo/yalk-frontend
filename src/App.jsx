8c8630334557f713fd628d53a97ed257f2c5560a
Revengeic3 <onair95@gmail.com>
2023-05-20 08:29:14 +0200

    Move to authenticated sessions

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,60 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Box, Center, Heading } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom';
+import ChatBrowser from './components/ChatBrowser/ChatBrowser';
 import ChatProvider from './context/ChatContext';
 import ChatLayout from './layouts/ChatLayout';
 import Chat from './pages/Chat';
 import Home from './pages/Home';
 import Login from './pages/Login';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <ChatLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/chat/:id',
-        element: <Chat />,
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-]);
+import AuthService from './services/auth.service';
 
 function App() {
-  return (
-    <ChatProvider url={'ws://localhost:8080/ws'}>
-      <RouterProvider router={router} />
-    </ChatProvider>
-  );
+  // ? State
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <ChatProvider url={'ws://localhost:8080/ws'}>
+          <ChatLayout />
+        </ChatProvider>
+      ),
+      loader: () => AuthService.validate(),
+      errorElement: <AppError />,
+      children: [
+        {
+          path: '/chat/:id',
+          element: <Chat />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
+
+export const AppError = () => {
+  const navigate = useNavigate();
+  const error = useRouteError();
+
+  useEffect(() => {
+    if (error.status !== undefined && error.status === 404) {
+      return
+    }
+
+    if (error.response.status && error.response.status === 401) {
+      navigate('/login');
+    }
+  }, []);
+};
 
 export default App;
