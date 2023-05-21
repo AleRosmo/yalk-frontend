885169d240595e5857e14ec3b19198168e81019a
Revengeic3 <onair95@gmail.com>
2023-05-21 21:20:32 +0200

    Move to Settings page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,15 @@
-import { Center, Flex, Heading, Text } from '@chakra-ui/react';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useOutletContext } from 'react-router-dom';
-import { MessageRow } from '../components/MessageRow/MessageRow';
-import { useAxiosFetch } from '../hooks/useAxiosFetch';
-import { useFetch } from '../hooks/useFetch';
-import { useRenderCount } from '../hooks/useRenderCount';
-// import { ChatBox } from '../layouts/ChatBox';
 
-function Home() {
+function Settings() {
   const context = useOutletContext();
 
   return <div>Home</div>;
-  // return <Text color={'white'}>{context.test}</Text>;
+
 }
 
 export const HomeLoader = () => {
   return <div>Home</div>;
 };
 
-export default Home;
+export default Settings;
