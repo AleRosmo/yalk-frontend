f222e4d695e815969a9c91d3d20e683455713348
Revengeic3 <onair95@gmail.com>
2023-05-23 18:54:33 +0200

    Move ChatService to Context

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -8,6 +8,8 @@ import {
   Textarea,
   useToast,
 } from '@chakra-ui/react';
+import { useChatService } from '../context/ChatContext';
+
 import React, {
   useCallback,
   useLayoutEffect,
@@ -18,107 +20,67 @@ import React, {
 import { useOutletContext, useParams } from 'react-router-dom';
 import { IconHeader } from '../components/IconHeader/IconHeader';
 import { MessageRow } from '../components/MessageRow/MessageRow';
-import { Message } from '../services/Chat/messages';
+// import { Message } from '../services/Chat/messages';
 
 export default function Chat() {
   const context = useOutletContext();
   const params = useParams();
   const toast = useToast();
   const chatContainer = useRef(null);
-  const [messageHistory, setMessageHistory] = useState(null);
+  // const [messageHistory, setMessageHistory] = useState(null);
   const [messageTextValue, setMessageTextValue] = useState();
+  const {
+    user,
+    chats,
+    accounts,
+    serverUsers,
+    sendMessage,
+    addAccount,
+    isLoading,
+  } = useChatService();
 
-  // const currentChat = useMemo(
-  //   context.chat.find(chat => chat.id === params.chatId),
-  //   [params.id]
-  // );
-
-  const currentChat = useMemo(() => {
-    return context.chats[parseInt(params.id)];
-  }, [params.id]);
+  const chat = chats.get(parseInt(params.id));
 
   // TODO: Custom hook?
   const handleKeyPress = event => {
     if (event.key === 'Enter') {
       event.preventDefault();
-
-      const message = new Message();
-      const timestamp = new Date();
-
-      message.new({
-        chatId: currentChat.id,
-        messageType: 'chat_message',
-        timestamp: timestamp.toISOString(),
-        content: messageTextValue,
-      });
-
-      context.sendMessage(message);
+      sendMessage(chat.id, messageTextValue);
       setMessageTextValue('');
     }
   };
 
-  const messageListener = useCallback(event => {
-    // const message = new Message();
-    const payload = JSON.parse(event.data);
-    if (payload.type !== 'chat_message') {
-      return;
-    }
+  // if () {
+  //   return (
+  //     <Center>
+  //       <IconHeader title={'No chats'} />
 
-    const message = payload.data;
+  //     </Center>
+  //   );
+  // }
 
-    toast({
-      title: 'Debug: Message received',
-      description: message.content,
-      status: 'info',
-      duration: 5000,
-      position: 'bottom-right',
-    });
-
-    if (message.chatId === currentChat.id) {
-      setMessageHistory(prevMessages => {
-        if (!prevMessages) {
-          return (
+  return (
+    <>
+      {/* TODO: Add invalid chat header error like above */}
+      <IconHeader title={chat.name} />
+      <Flex h="full" flexDir={'column'} overflow={'scroll'} ref={chatContainer}>
+        {chat.messages.length === 0 ? (
+          <MessageRow
+            key={1}
+            user={serverUsers.get(1)}
+            content={`It's quiet here! write something first!`}
+            isLastMessage={true}
+          />
+        ) : (
+          chat.messages.map((message, index) => (
             <MessageRow
               key={message.id}
               user={message.user}
               content={message.content}
-              isLastMessage={true}
+              isLastMessage={index === chat.messages.length - 1}
             />
-          );
-        }
-
-        return [
-          ...prevMessages,
-          <MessageRow
-            key={message.id}
-            user={message.user}
-            content={message.content}
-            isLastMessage={true}
-          />,
-        ];
-      });
-    }
-  });
-
-  useLayoutEffect(() => {
-    if (currentChat) {
-      setMessageHistory(() => makeChatRows(currentChat));
-      context.websocket.addEventListener('message', messageListener);
-
-      return () => {
-        setMessageHistory(null);
-        context.websocket.removeEventListener('message', messageListener);
-      };
-    }
-  }, [params.id]);
-
-  return currentChat ? (
-    <>
-      <Skeleton isLoaded={currentChat}>
-        <IconHeader title={currentChat ? currentChat.name : 'No chats'} />
-      </Skeleton>
-      <Flex h="full" flexDir={'column'} overflow={'scroll'} ref={chatContainer}>
-        {messageHistory}
+          ))
+        )}
         <Spacer />
       </Flex>
       <Box margin={'10px'}>
@@ -131,28 +93,10 @@ export default function Chat() {
           onChange={e => setMessageTextValue(e.target.value)}
           value={messageTextValue}
           onKeyDown={handleKeyPress}
-          placeholder={'Uhm..'}
+          placeholder={'Type something..'}
           _placeholder={{ color: 'teal.700' }}
         />
       </Box>
     </>
-  ) : (
-    <Center>
-      <Heading color={'teal'}>{'No chats to display!'}</Heading>
-    </Center>
   );
-}
-
-function makeChatRows(chat) {
-  if (!chat.messages || chat.messages.length === 0) {
-    return null;
-  }
-  return chat.messages.map((message, index) => (
-    <MessageRow
-      key={message.id}
-      user={message.user}
-      content={message.content}
-      isLastMessage={index === chat.messages.length - 1}
-    />
-  ));
 }
--- src/services/Chat/Account.js
+++
@@ -1,9 +0,0 @@
-export default class Account {
-  constructor({ ID = null, email = null, username = null, password = null }) {
-    this.ID = ID;
-    this.email = email;
-    this.username = username;
-    this.password = password;
-  }
-  
-}
