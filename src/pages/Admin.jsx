36621832500955c8a5a4c05beb026859e10bfc7a
Revengeic3 <onair95@gmail.com>
2023-05-26 04:40:29 +0200

    Strip away event handling logic

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -6,93 +6,40 @@ import {
   TableContainer,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react';
-import { useOutletContext } from 'react-router-dom';
-
-import React, {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useState,
-} from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { Header } from '../components/Header/Header';
 import UserModal from '../components/UserModal/UserModal';
-import Account from '../services/Chat/Account';
+import { useChatService } from '../context/ChatServiceContext';
 
 const Admin = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const chatService = useOutletContext();
-  const [accountRows, setAccountsRows] = useState([]);
-
-  const messageListener = useCallback(event => {
-    // const message = new Message();
-    const payload = JSON.parse(event.data);
-    if (payload.type !== 'chat_message') {
-      return;
-    }
-
-    const message = payload.data;
-
-    if (message.chatId === currentChat.id) {
-      setAccountsRows(prevRows => {
-        if (!prevRows) {
-          return (
-            <TableRow
-              key={account.ID}
-              id={account.ID}
-              username={account.username}
-              email={account.email}
-            />
-          );
-        }
-
-        return [
-          ...prevRows,
-          <TableRow
-            key={account.ID}
-            id={account.ID}
-            username={account.username}
-            email={account.email}
-          />,
-        ];
-      });
-    }
-  });
-
-  useEffect(() => {
-    const accounts = chatService.getAccounts();
-    const accountsArray = [];
-    accounts.forEach(account => {
-      accountsArray.push(
-        <TableRow
-          key={account.ID}
-          id={account.ID}
-          username={account.username}
-          email={account.email}
-        />
-      );
-    });
-
-    setAccountsRows(accountsArray);
-
-    return () => {
-      setAccountsRows([]);
-    };
-  }, [chatService]);
+  const { accounts, addAccount } = useChatService();
+  const toast = useToast();
+  const userAdded = useRef(false);
 
   // TODO: Maybe just a method with args on the payload is ok
   const handleCreateAccount = useCallback((email, username, password) => {
-    const account = new Account({
-      email: email,
-      username: username,
-      password: password,
-    });
-    chatService.addAccount(account);
+    addAccount(email, username, password);
+    userAdded.current = true;
   });
+
+  useEffect(() => {
+    userAdded.current
+      ? toast({
+          title: 'Accounts created',
+          status: 'success',
+          duration: 5000,
+          position: 'bottom-right',
+        })
+      : null;
+  }, [accounts]);
 
   return (
     <>
@@ -104,7 +51,16 @@ const Admin = () => {
             <Thead>
               <TableHeader />
             </Thead>
-            <Tbody>{accountRows}</Tbody>
+            <Tbody>
+              {accounts.map(account => (
+                <TableRow
+                  key={account.ID}
+                  id={account.ID}
+                  username={account.username}
+                  email={account.email}
+                />
+              ))}
+            </Tbody>
           </Table>
         </TableContainer>
       </Flex>
@@ -130,7 +86,7 @@ const TableHeader = () => (
   </Tr>
 );
 
-const TableRow = ({ id, username, email, displayedName }) => (
+const TableRow = ({ id, username, email }) => (
   <Tr>
     {/* <Td>
       <Avatar size="sm" src={avatar} />
